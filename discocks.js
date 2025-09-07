74536688db841699044c89c10667a766572eac64
Valter Nestor <ValterZed.07@gmail.com>
2025-09-07 20:41:46 +0200

    Added reqwuirements

--- discocks.js
+++ discocks.js
@@ -2,7 +2,6 @@ var Discogs = require('disconnect').Client;
 const express = require('express');
 const app = express();
 const cors = require("cors");
-const { format } = require('path');
 app.use(cors());
 app.use(express.json());
 
